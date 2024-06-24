589e01f54dd8b4183e62c2614f0f78af3eefddb9
Dharini M <92968706+dharini03@users.noreply.github.com>
2024-06-24 15:36:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function searchRecipes(){
+    let search = document.getElementById("search").value;
+    let search1 = document.getElementById("search1").value;
+    let search2 = document.getElementById("search2").value;
+    let xhr = new XMLHttpRequest();
+}
